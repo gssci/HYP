4a026f6c75d8399df087df4451eafd7eade4eb36
Gerlando Savio Scibetta <scibetta.savio@gmail.com>
2016-06-06 18:25:29 +0200

    removed php from pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
+$(document).ready(function(){
+    
+    $.get("/HYP/includes/navbar.html", function(data){
+    $("body").prepend(data);
+});
+     
+    $.get("/HYP/includes/footer.html", function(data){
+    $("body").append(data);
+});
+    
+});
