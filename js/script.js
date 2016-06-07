b5a6706a482f3af4367d1782885fa2393789f070
Gerlando Savio Scibetta <scibetta.savio@gmail.com>
2016-06-07 20:52:38 +0200

    prova ancora

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
+    $(document).ready(function(){
+    
+        $.ajax({url: "includes/navbar.html", success: function(result){
+            $("body").prepend(result);
+        }});
+    
+});
