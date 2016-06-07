db977e97892bcc64a1c8c707642bce58db8f672e
Gerlando Savio Scibetta <scibetta.savio@gmail.com>
2016-06-07 21:07:39 +0200

    fixed issue with inclusion (AJAX)

--- js/script.js
+++ js/script.js
@@ -3,5 +3,9 @@
         $.ajax({url: "includes/navbar.html", success: function(result){
             $("body").prepend(result);
         }});
+        
+        $.ajax({url: "includes/footer.html", success: function(result){
+            $("body").append(result);
+        }});
     
 });
