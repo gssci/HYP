819fc1824baebbe7bcfade67b0248827d1a9a2e4
Gerlando Savio Scibetta <scibetta.savio@gmail.com>
2016-06-09 17:59:20 +0200

    maybe this time

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-    $(document).ready(function(){
-        $.support.cors=true; //enables cross origin resource sharing.
-        
-        $.ajax({url: "includes/navbar.html", success: function(result){
-            $("body").prepend(result);
-        }});
-        
-        $.ajax({url: "includes/footer.html", success: function(result){
-            $("body").append(result);
-        }});
-    
-});
+        $(document).bind("mobileinit", function () {
+            $.mobile.allowCrossDomainPages = true;
+        });
+
+        $(document).ready(function () {
+            $.support.cors = true; //enables cross origin resource sharing.
+
+
+
+            $.ajax({
+                url: "includes/navbar.html",
+                success: function (result) {
+                    $("body").prepend(result);
+                }
+            });
+
+            $.ajax({
+                url: "includes/footer.html",
+                success: function (result) {
+                    $("body").append(result);
+                }
+            });
+
+        });
